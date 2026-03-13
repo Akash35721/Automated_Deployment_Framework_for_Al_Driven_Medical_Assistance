a1916dbef9ee7a90ee20e2e32f4edfe224c22ec1
Akash35721 <2004akashchauhan@gmail.com>
2026-03-14 05:03:18 +0530

    fix old 1

--- iam.tf
+++ iam.tf
@@ -1,11 +1,8 @@
 # Creates an IAM role for the EC2 instance
-<<<<<<< HEAD
+
 resource "aws_iam_role" "major2357_ec2_role" {
   name = "major2357-EC2-Role1"
-=======
-resource "aws_iam_role" "major2_ec2_role" {
-  name = "major2-EC2-Role"
->>>>>>> 22731154287d3879e31b0a411e6d6bcbabdc19ba
+
 
   # The policy that allows EC2 to assume this role
   assume_role_policy = jsonencode({
@@ -22,13 +19,9 @@ resource "aws_iam_role" "major2_ec2_role" {
   })
 }
 # Creates an instance profile that can be associated with the EC2 instance
-<<<<<<< HEAD
+
 resource "aws_iam_instance_profile" "major2357_instance_profile" {
   name = "major2357-EC2-Instance-Profile1"
   role = aws_iam_role.major2357_ec2_role.name
-=======
-resource "aws_iam_instance_profile" "major2_instance_profile" {
-  name = "major2-EC2-Instance-Profile"
-  role = aws_iam_role.major2_ec2_role.name
->>>>>>> 22731154287d3879e31b0a411e6d6bcbabdc19ba
+
 }
