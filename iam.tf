8616e434438a9bb3a929d2ffa5f477e1a11acc06
Akash35721 <2004akashchauhan@gmail.com>
2026-03-14 04:55:34 +0530

    feat: Add GitHub Actions CI/CD pipeline for Terraform provisioning and Docker deployment, update Terraform resource names from `major1` to `major2357`, and ignore `major1.

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 # Creates an IAM role for the EC2 instance
-resource "aws_iam_role" "major1_ec2_role" {
-  name = "major1-EC2-Role1"
+resource "aws_iam_role" "major2357_ec2_role" {
+  name = "major2357-EC2-Role1"
 
   # The policy that allows EC2 to assume this role
   assume_role_policy = jsonencode({
@@ -17,7 +17,7 @@ resource "aws_iam_role" "major1_ec2_role" {
   })
 }
 # Creates an instance profile that can be associated with the EC2 instance
-resource "aws_iam_instance_profile" "major1_instance_profile" {
-  name = "major1-EC2-Instance-Profile1"
-  role = aws_iam_role.major1_ec2_role.name
+resource "aws_iam_instance_profile" "major2357_instance_profile" {
+  name = "major2357-EC2-Instance-Profile1"
+  role = aws_iam_role.major2357_ec2_role.name
 }
